4710abd55c3865d578c8d9b07da5cb0110905e8f
Arnav singh <arnav.12.singh.2006@gmail.com>
2025-10-02 14:03:24 +0530

    Add files via upload

---
+++ eqality.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float a,b;
+    printf("enter the value of a and b");
+    scanf("%f %f", &a , &b );
+    if (a==b)
+    printf("a is equal to b");
+    else
+    printf("a is not equal to b");
+    return 0;
+}
